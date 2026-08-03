3e1fdc0e20407e32a437d27a517d360eaf223161
Fanorisky <fanorisky168@gmail.com>
2026-08-04 00:30:39 +0700

    Improve Pawn syntax highlighting

--- languages/pawn/highlights.scm
+++ languages/pawn/highlights.scm
@@ -1,30 +1,52 @@
 ; Generic identifiers are the fallback; specific roles below take precedence.
 (identifier) @variable
 
-; Definitions and calls
+; Definitions, references, and calls
 (function_definition name: (identifier) @function)
 (function_declaration name: (identifier) @function)
+(field_access field: (identifier) @property)
+(call_expression
+  function: (field_access
+    field: (identifier) @function))
 (call_expression
   function: (primary_expression
     (identifier) @function))
-(call_expression function: (field_access field: (identifier) @function))
-(field_access field: (identifier) @property)
+(label_statement name: (identifier) @label)
 
 ; Parameters and variables
 (parameter_declaration name: (identifier) @variable.parameter)
 (variable_declarator name: (identifier) @variable)
 
-; Types and tags
+; Types, tags, enums, and constants
 (builtin_type) @type.builtin
-(tagged_type tag: (identifier) @type)
+(tagged_type
+  tag: (identifier) @type
+  ":" @type)
 (type (identifier) @type)
+(enum_declaration name: (identifier) @type)
+(enum_entry name: (identifier) @constant)
 
-; Preprocessor
-(preprocessor) @preproc
-(preproc_include) @include
-(preproc_define) @preproc
-(preproc_define name: (identifier) @constant)
-(preproc_undef name: (identifier) @constant)
+; Preprocessor directives
+(preproc_include
+  "#" @punctuation.special
+  ["include" "tryinclude"] @keyword)
+(preproc_define
+  "#" @punctuation.special
+  "define" @keyword
+  name: (identifier) @constant)
+(preproc_undef
+  "#" @punctuation.special
+  "undef" @keyword
+  name: (identifier) @constant)
+(preproc_conditional
+  "#" @punctuation.special
+  ["if" "elseif" "else" "endif" "ifdef" "ifndef"] @keyword)
+(preproc_diagnostic
+  "#" @punctuation.special
+  ["assert" "error" "warning" "pragma" "emit" "endinput"] @keyword)
+(preproc_directive
+  "#" @punctuation.special
+  directive: (identifier) @keyword)
 (system_lib_string) @string
 
 ; Statements and control flow
@@ -40,13 +62,6 @@
   "default"
 ] @keyword
 
-(for_statement) @keyword
-(foreach_statement) @keyword
-(while_statement) @keyword
-(do_while_statement) @keyword
-(switch_statement) @keyword
-(ternary_expression) @keyword
-
 ; Pawn modifiers and language keywords
 [
   "new"
@@ -57,6 +72,7 @@
   "forward"
   "native"
   "inline"
+  "using"
   "hook"
   "task"
   "ptask"
@@ -71,7 +87,13 @@
   "assert"
 ] @keyword
 
-["sizeof" "tagof"] @function.builtin
+["sizeof" "tagof"] @operator
+
+; Context-sensitive punctuation
+(ternary_expression ["?" ":"] @operator)
+(case_clause ":" @punctuation.delimiter)
+(default_clause ":" @punctuation.delimiter)
+(label_statement ":" @punctuation.delimiter)
 
 ; Operators and punctuation
 [
@@ -79,12 +101,15 @@
   "+" "-" "*" "/" "%" "++" "--"
   "==" "!=" "<>" "<" ">" "<=" ">="
   "!" "&&" "||" "&" "|" "~" "^" "<<" ">>"
-  "?" ":"
-] @operator
+ ] @operator
 
 ["." "->" ","] @punctuation.delimiter
-(dimension) @punctuation.bracket
-(array_literal) @punctuation.bracket
+(dimension
+  "[" @punctuation.bracket
+  "]" @punctuation.bracket)
+(array_literal
+  "{" @punctuation.bracket
+  "}" @punctuation.bracket)
 
 ; Literals and comments
 (comment) @comment
